bc96a08a1f62cf62381697633886374395ff4858
TAKE Takashi <nakamizo.takashi@gmail.com>
2025-07-18 16:31:04 +0900

    init修正

--- AudioInfoExtractor/__init__.py
+++ AudioInfoExtractor/__init__.py
@@ -1,1 +1,8 @@
-from .audio_info_extractor import AudeeInfoExtractor, BitfanInfoExtractor, AudioInfo, get_extractor
+from .audio_info_extractor import (
+    AudeeInfoExtractor,
+    AudioInfo,
+    BitfanInfoExtractor,
+    get_extractor,
+)
+
+__all__ = ["AudeeInfoExtractor", "BitfanInfoExtractor", "AudioInfo", "get_extractor"]
