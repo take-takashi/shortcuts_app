514d4131056f861a3ac359b802e732913c62ce31
TAKE Takashi <puller.swipes1h@icloud.com>
2025-08-14 23:59:28 +0900

    コメントを削除

--- MyNotionHelper/my_notion_helper.py
+++ MyNotionHelper/my_notion_helper.py
@@ -270,9 +270,6 @@ class MyNotionHelper:
             なし
         """
 
-        # TODO: 5GBを超える場合は、パートに分割してアップロードしたい（優先度：低　ffmpg使用か）
-        # TODO: アップロードする際に判断すればいい
-
         try:
             # 20MB以下ならsingle_part、20MB超ならmulti_partとする（Notion APIの仕様）
             file_size = os.path.getsize(file_path)
